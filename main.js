66a6fc36268f984b86f647dce8ff9cbb3314eabf
k0j9h03 <k0j9h03@naver.com>
2024-03-10 21:11:08 +0900

    test

---
+++ main.js
@@ -0,0 +1,33 @@
+let sectionNumber = 0;
+
+/** 스크롤 섹션넘버 체크하는 함수 */
+function checkSectionNumber() {
+    const scrollY = window.scrollY;
+    let sectionHeight1 = document.querySelector('.section1').offsetHeight;
+    let sectionHeight2 = document.querySelector('.section2').offsetHeight + sectionHeight1;
+    let sectionHeight3 = document.querySelector('.section3').offsetHeight + sectionHeight2;
+    let sectionHeight4 = document.querySelector('.section4').offsetHeight + sectionHeight3;
+    
+    if (scrollY < sectionHeight1) {
+        sectionNumber = 1;
+    } else if (scrollY >= sectionHeight1 && scrollY < sectionHeight2) {
+        sectionNumber = 2;
+    } else if (scrollY >= sectionHeight2 && scrollY < sectionHeight3) {
+        sectionNumber = 3;
+    } else if (scrollY >= sectionHeight3 && scrollY < sectionHeight4) {
+        sectionNumber = 4;
+    }
+    
+    console.log(sectionNumber);
+
+
+    if (sectionNumber == 3){
+        let pointPercentage = ((scrollY - sectionHeight2) / ((sectionHeight3 - sectionHeight2) - window.innerHeight)) * 100;
+        let reversePointPercentage = 100-pointPercentage;
+        document.querySelector('.image2').style.clipPath = `inset(${reversePointPercentage}% 0% 0% 0%)`;
+    }
+}
+
+window.addEventListener('scroll', checkSectionNumber);
+
+
